e8e6c7627f2818a2ad7471c47fe6f94eb1d8d964
Ramon <ramonsosa3000@outlook.com>
2018-05-13 16:53:58 -0400

    Implementado el Card View en la vista Product_List

--- app/src/main/java/com/android/example/mimercado/MainActivity.java
+++ app/src/main/java/com/android/example/mimercado/MainActivity.java
@@ -1,5 +1,6 @@
 package com.android.example.mimercado;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,15 +19,16 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                    startActivity(new Intent(MainActivity.this,ProductList.class));
             }
         });
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
